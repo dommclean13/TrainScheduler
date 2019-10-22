efafecc5e065376a37762431b0b31830afa116cb
Dominique McLean <dommclean13@gmail.com>
2019-10-22 17:02:20 -0500

    changed html and js

--- asset/Javascript/app.js
+++ asset/Javascript/app.js
@@ -1,36 +1,45 @@
-$(document).ready(function () {
-
     // Initialize Firebase
-    var firebaseConfig = {
-        apiKey: "AIzaSyBZFb6PI6bOxBfG0dv4ttHpdZ__MQngzKc",
-        authDomain: "traintracker-b208f.firebaseapp.com",
-        databaseURL: "https://traintracker-b208f.firebaseio.com",
-        projectId: "traintracker-b208f",
-        storageBucket: "traintracker-b208f.appspot.com",
-        messagingSenderId: "418472813221",
-        appId: "1:418472813221:web:f6496c6f84bade9db7634c",
-        measurementId: "G-T3ERWDV4PZ"
-      };
-    firebase.initializeApp(firebaseConfig);
-  
+    var config = {
+      apiKey: "AIzaSyBZFb6PI6bOxBfG0dv4ttHpdZ__MQngzKc",
+      authDomain: "traintracker-b208f.firebaseapp.com",
+      databaseURL: "https://traintracker-b208f.firebaseio.com",
+      projectId: "traintracker-b208f",
+      storageBucket: "traintracker-b208f.appspot.com",
+      messagingSenderId: "418472813221",
+      appId: "1:418472813221:web:f6496c6f84bade9db7634c",
+      measurementId: "G-T3ERWDV4PZ"
+    };
+
+    firebase.initializeApp(config);
+
+    var trainName 
+    var destination
+    var firstTrain 
+    var freq
+
+
+    $(document).ready(function () {
+
     var database = firebase.database();
+
+
   
     // Capture Button Click
     $("#newTrain").on("click", function (event) {
       event.preventDefault();
   
       // Values from text boxes
-      var trainName = $("#trainName").val();
-      var destination = $("#destination").val();
-      var firstTrain = $("#firstTrain").val();
-      var freq = $("#interval").val();
-      
+      var formTrainName = $("#trainName").val();
+      var formDestination = $("#destination").val();
+      var formFirstTrain = $("#firstTrain").val();
+      var formFreq = $("#interval").val();
+
       // Code for handling the push
       database.ref().push({
-        trainName: trainName,
-        destination: destination,
-        firstTrain: firstTrain,
-        frequency: freq
+        trainName: formTrainName,
+        destination: formDestination,
+        firstTrain: formFirstTrain,
+        frequency: formFreq,
       });
     });
   
@@ -44,8 +53,8 @@ $(document).ready(function () {
       var newFreq = childSnapshot.val().frequency;
   
       // First Time (pushed back 1 year to make sure it comes before current time)
-      var startTimeConverted = moment(newFirstTrain, "hh:mm").subtract(1, "years");
       var currentTime = moment();
+      var startTimeConverted = moment(newFirstTrain, "hh:mm").subtract(1, "years");
       var diffTime = moment().diff(moment(startTimeConverted), "minutes");
       var tRemainder = diffTime % newFreq;
       var tMinutesTillTrain = newFreq - tRemainder;
@@ -53,21 +62,20 @@ $(document).ready(function () {
       var catchTrain = moment(nextTrain).format("HH:mm");
   
 
+        console.log(currentTime.format("hh:mm"))
+
       // Display On Page
       $("#showAll").append(
         ' <tr><td>' + newTrain +
         ' </td><td>' + newLocation +
         ' </td><td>' + newFreq +
         ' </td><td>' + catchTrain +
-        ' </td><td>' + tMinutesTillTrain + ' </td></tr>');
+        ' </td><td>' + tMinutesTillTrain + ' </td></tr>')
   
       // Clear input fields
       $("#trainName, #destination, #firstTrain, #interval").val("");
       return false;
-    },
-      //Handle the errors
-      function (errorObject) {
-        console.log("Errors handled: " + errorObject.code);
-      });
-  
+    });
+    
+     
   }); 
